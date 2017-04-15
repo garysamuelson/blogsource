79489862cbfd6421d10b72aa329451d770353c17
Gary Samuelson <garys@garysamuelson.com>
2017-04-15 07:04:30 -0500

    checkin before code churn

--- camundarest/src/main/java/com/talkdata/task/PrintProcessVars.java
+++ camundarest/src/main/java/com/talkdata/task/PrintProcessVars.java
@@ -4,9 +4,14 @@ import java.util.Iterator;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.camunda.bpm.engine.CaseService;
+import org.camunda.bpm.engine.delegate.DelegateCaseExecution;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.DelegateTask;
+import org.camunda.bpm.engine.impl.cmmn.entity.runtime.CaseExecutionEntity;
 
 @Named("printProcessVars")
 public class PrintProcessVars {
@@ -16,6 +21,8 @@ public class PrintProcessVars {
 	 */
 	private final Logger LOGGER = Logger.getLogger(PrintProcessVars.class.getName());
 	
+  @Inject
+  private CaseService caseService;
 	
 	
 	/**
@@ -59,6 +66,83 @@ public class PrintProcessVars {
 	}
 	
 	
+	/**
+	 * This is now working - need to use "Expression" of listener type. 
+	 * 
+	 * NOT! - this isn't fetching the case variables correctly
+	 * 
+	 * @param caseExecutionEntity
+	 * @throws Exception
+	 */
+  public void printCaseVariables(DelegateCaseExecution execution) throws Exception {
+
+    LOGGER.info("*** caseTest");
+    
+    if(execution == null) {
+      LOGGER.info("*** execution is null!!!");
+      return;
+    }
+    
+    
+    
+    // =========================================
+    
+    LOGGER.info("*******************");
+    LOGGER.info("*** CaseDefinitionId ID: "+execution.getCaseDefinitionId()); // getProcessDefinitionId())
+    LOGGER.info("*** CaseInstanceId ID: " + execution.getCaseInstanceId());
+    
+    LOGGER.info("*** ActivityId ID: "+execution.getActivityId());  // getCurrentActivityId());
+
+    // LOGGER.info("*** Has variables: "+ execution.hasVariables());
+
+    
+    Map<String, Object> processVars = execution.getVariables();
+    //Map<String, Object> processVars = caseService.getVariables(execution.);
+    Iterator<Map.Entry<String,Object>> entries = processVars.entrySet().iterator();
+
+    if (processVars.isEmpty()) {
+      LOGGER.info("*** Process Variables: EMPTY");
+    } else {
+      LOGGER.info("*** Process Variables: ");
+    } 
+
+    // Print out all the variables. 
+    while (entries.hasNext()) {
+      Map.Entry<String, Object> thisEntry = entries.next();
+      Object object = thisEntry.getValue();
+      if (object != null) {
+        LOGGER.info(thisEntry.getKey().toString() + " : "+object.toString()); 
+      } else {
+        LOGGER.info(thisEntry.getKey().toString() + " : null");
+      }
+    }
+    java.util.Date date = new java.util.Date();
+    LOGGER.info(">> Date Stamp: "+date);
+    //java.util.Date date = new java.util.Date();
+    LOGGER.info("*******************");
+    
+    
+
+  }
 	
+	
+	
+	/**
+	 * 
+	 * 
+	 * @throws Exception
+	 */
+	public void ping() throws Exception {
+	  
+	  LOGGER.info("*** ping");
+	  
+	  
+	}
+	
+	
+  public void methodOne(DelegateTask task) {
+    LOGGER.info("*** methodOne");
+    
+  }
 
 }
